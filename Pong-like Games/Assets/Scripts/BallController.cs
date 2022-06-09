9f767b7e40a8bae798d0a35b0c8f73255ced8f4b
Ahmad Dedi Sutrisno <ahmaddedi7@gmail.com>
2022-06-09 07:04:53 +0700

    Base gameplay

---
+++ Pong-like Games/Assets/Scripts/BallController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public Vector2 speed;
+
+    private Rigidbody2D rig;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        rig.velocity = speed;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        GetComponent<Transform>();
+        Vector3 pos = transform.position;
+        //transform.Translate(speed * Time.deltaTime);
+    }
+}
